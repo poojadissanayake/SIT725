667452fe9228b9ab2d06a24b943828ccf74fc877
Pooja Dissanayake <poojadissanayake6@gmail.com>
2024-07-19 16:23:46 +1000

    POST call

--- week2/server_get.js
+++ week2/server_get.js
@@ -5,6 +5,7 @@ const express= require("express");
         const addTwoNumber= (n1,n2) => {
             return n1+n2;
         }
+        const subtractNumbers = (n1, n2) => n1 - n2;
     
         app.get("/addTwoNumber", (req,res)=>{
             const n1= parseInt(req.query.n1);
@@ -12,6 +13,13 @@ const express= require("express");
             const result = addTwoNumber(n1,n2);
             res.json({statuscocde:200, data: result }); 
         });
+
+        app.post("/subtractNumbers", (req, res) => {
+            const n1= parseInt(req.query.n1);
+            const n2=parseInt(req.query.n2);
+            const result = subtractNumbers(n1, n2);
+            res.json({ statusCode: 200, data: result });
+          });
     
         app.get("/Display", (req, res) => {
             const n1 = "<html><body><H1>HELLO THERE </H1></body></html>";
